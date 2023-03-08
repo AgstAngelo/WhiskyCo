d26b80d8ebccfbd7e3002f9a2f9a80d88e7bb323
MBoneli92 <116234582+MBoneli92@users.noreply.github.com>
2023-03-08 10:50:30 -0300

    Update App.ts

--- src/infra/App.ts
+++ src/infra/App.ts
@@ -2,14 +2,6 @@ import Express, { Application, Request, Response, NextFunction } from "express";
 import { mongoDB } from "../database";
 import BaseRoutes from "./BaseRoutes";
 
-const app: Application = Express();
-
-app.use((req: Request, res: Response, next: NextFunction) => {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE,OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-  next();
-});
 
 type SetupOptions = {
   isTest?: boolean;
