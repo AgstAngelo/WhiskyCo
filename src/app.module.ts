0e67017953176bbdf93ecc93f7e3483a83bdb5d2
AgstAngelo <agstangelo@gmail.com>
2023-05-09 20:49:40 -0300

    feat: added prisma module
    added module and service.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './User/user.module';
 import { AuthModule } from './Auth/auth.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [UserModule, AuthModule],
+  imports: [UserModule, AuthModule, PrismaModule],
 })
 
 export class AppModule {}
